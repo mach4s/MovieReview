accfe3c96df5bdacec48a04816b7dea63ccae8f3
Saliya Mach <saliyamach@yahoo.com>
2018-12-15 18:52:36 -0800

    testing

---
+++ MovieReview/js/genre.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+
+    getMovies("Happy");
+
+});
+
+
+function getMovies(searchText){
+  axios.get("http://www.omdbapi.com/?apikey=6c1b77b8&s=" + searchText)
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
